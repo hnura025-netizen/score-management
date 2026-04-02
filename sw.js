6ed661f1ab6230b9ed6714672219ec10467cce90
hnura025-netizen <hnura025@gmail.com>
2026-04-02 03:08:29 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ggss-portal-v1';
+const ASSETS = [
+  '/',
+  'index.html',
+  'icon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
